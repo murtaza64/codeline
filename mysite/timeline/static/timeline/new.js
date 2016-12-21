51e25a18b589dbdbd4a2545430b7f9f4d8358799
User <user@Mohameds-iMac.local>
2016-12-21 16:35:18 +0400

    Implemented working Ace editor in /new
    Not properly tested; no idea what could break it.

--- mysite/timeline/static/timeline/new.js
+++ mysite/timeline/static/timeline/new.js
@@ -1,22 +1,29 @@
-cellinput_html = '\
+CELLINPUT_HTML = '\
 <div class="container-fluid cellfield" >\
   <div class="cellinputmeta">\
     <table><tr>\
-      <td><button class="cellinputtype" style="width: 35px;">Aa</button></td>\
+      <td><button class="cellinputtype">Aa</button></td>\
       <td style="width: 100%; padding-left:5px">\
         <input type=text class="newfield cellinputname">\
       </td>\
+      <td>\
+        <input type=text class="newfield cellinputlang">\
+      </td>\
     </tr></table>\
   </div>\
-  <textarea rows=2 class="newfield cellinputcontent"></textarea>\
-</div>\
-'
+  <div class="cic_container">\
+      <div class = ace></div>\
+  </div>\
+</div>';
 // using jQuery
+
+TEXTAREA_HTML = '<textarea rows=2 class="newfield cellinputcontent"></textarea>';
 
 var CELL_NAME = "cell name";
 var CELL_CONTENT = "cell content";
 var TAGS = "tag1 tag2 ...";
 var POST_TITLE = "post title";
+var LANG = "language"
 
 function getCookie(name) {
     var cookieValue = null;
@@ -59,15 +66,20 @@ $.ajaxSetup({
 $(function(){
   console.log('ready');
   $('.newfield').css('color', '#909090');
-  setup_inputs()
+  setup_inputs(0);
+  setup_ace(0);
+  setup_placeholder("#titlefield", POST_TITLE);
+  setup_placeholder("#tagfield", TAGS);
 
   $('#addcell').click(function (){
     var cellinputs = $("#cellinputs");
-    cellinputs.append(cellinput_html);
+    cellinputs.append(CELLINPUT_HTML);
     //cellfields = $(".cellfield");
     //$(cellfields[cellfields.length-1]).css("margin-bottom", "0");
     //$(cellfields[cellfields.length-2]).css("margin-bottom", "10px");
-    setup_inputs()
+    var i = $('.cellfield').length-1;
+    setup_ace(i);
+    setup_inputs(i);
   });
 
   $('#submit').click(function(){
@@ -82,15 +94,18 @@ $(function(){
     post.cells = [];
     var cellfields = $('.cellfield');
     var cellinputnames = $('.cellinputname');
-    var cellinputcontents = $('.cellinputcontent');
     var cellinputtypes = $('.cellinputtype');
+    var cellinputlangs = $('.cellinputlang');
     for (var i = 0; i < cellfields.length; i++){
       cell = {}
       cellfield = cellfields[i]
       cell.title = cellinputnames[i].value;
       if (cell.title == CELL_NAME) cell.title = "";
-      cell.content = cellinputcontents[i].value;
+      var editor = ace.edit("ace_editor" + i.toString());
+      cell.content = editor.getValue();
       if (cell.content == CELL_CONTENT) cell.content = "";
+      cell.lang = cellinputlangs[i]
+      if (cell.lang == LANG) cell.lang = "";
       type_btn = cellinputtypes[i]
       if (type_btn.innerHTML == 'Aa'){
         cell.type = 0;
@@ -136,24 +151,61 @@ $(function(){
     });
   });
 });
+function setup_ace(i){
+  var editor_id = "ace_editor" + i.toString();
+  $('.ace')[i].id = editor_id;
+  var editor = ace.edit(editor_id);
+  editor.setTheme("ace/theme/github");
+  editor.setOptions({
+    minLines: 5,
+    maxLines: 50
+  });
+  editor.getSession().setMode("ace/mode/text");
+  editor.getSession().setUseSoftTabs(true);
 
-function setup_inputs(){
+  editor.getSession().on("changeMode", function(){
+    console.log('changed mode');
+    $($(".cellinputlang")[i]).css("border-color", "#409060");
+    $(".cellinputlang")[i].last_correct_mode = $(".cellinputlang")[i].value.toLowerCase();
+  });
+}
+function setup_placeholder(selector, plchldr){
+  $(selector).focus(function(){
+    if (this.value == plchldr){
+      this.value = "";
+    }
+    $(this).css('color', 'inherit');
+  }).focusout(function(){
+    if (this.value == ""){
+      $(this).css('color', '#909090');
+      this.value = plchldr;
+    }
+  })
+
+    $(selector).val(plchldr);
+    $(selector).css("color", "#909090");
+}
+function setup_inputs(i){
   //$('.newfield').prop('contenteditable',true);
-  $('.cellinputtype').click(function(){
+  $($('.cellinputtype')[i]).click(function(){
     var i = $('.cellinputtype').index(this)
     if (this.innerHTML == 'Aa'){
       this.innerHTML = 'MD';
+      $($('.cellinputlang')[i]).val("markdown").attr("disabled", true);
     } else if (this.innerHTML == 'MD'){
       this.innerHTML = '{}';
       this.style.paddingTop = 0;
-      $('.cellinputcontent')[i].style.fontFamily = "Consolas,monospace";
       this.style.fontFamily = "Consolas,monospace";
+      $($('.cellinputlang')[i]).val("language").attr("disabled", false).css('color', '#909090');
+
     } else {
       this.style.paddingTop = 2;
       this.innerHTML = 'Aa';
-      $('.cellinputcontent')[i].style.fontFamily = "inherit";
+      $($('.cellinputlang')[i]).val("text").css("color", "inherit").attr("disabled", true);
+      //$('.cellinputcontent')[i].style.fontFamily = "inherit";
       this.style.fontFamily = "inherit";
     }
+    $($('.cellinputlang')[i]).change();
   });
 
   $('textarea').each(function () { //from Obsidian on StackOverflow
@@ -162,35 +214,27 @@ function setup_inputs(){
       this.style.height = 'auto';
       this.style.height = (this.scrollHeight) + 'px';
   });
-
-
-  function setup_placeholder(selector, plchldr){
-    $(selector).focus(function(){
-      if (this.value == plchldr){
-        this.value = "";
+  setup_placeholder($('.cellinputname')[i], CELL_NAME);
+  setup_placeholder($('.cellinputlang')[i], LANG);
+  $($('.cellinputlang')[i]).change(function(){
+    mode = this.value.toLowerCase()
+      if (mode == this.last_correct_mode) {
+        this.style.borderColor = "#409060";
+        return;
       }
-      $(this).css('color', 'inherit');
-    }).focusout(function(){
-      if (this.value == ""){
-        $(this).css('color', '#909090');
-        this.value = plchldr;
+      this.style.borderColor = "#c06060";
+      console.log('cellinputlang change');
+      var editor = ace.edit("ace_editor" + i.toString());
+      if (this.value == LANG || this.value == ""){
+        this.style.borderColor = "#d8d8d8";
+        return;
       }
-    })
-    sel = $(selector);
-    for (var i=0; i<sel.length; i++){
-      //console.log(sel[i], sel[i].value);
-      if (sel[i].value == ""){
-        sel[i].value = plchldr;
-        //console.log(sel[i], sel[i].value);
+      if (mode == "c" || mode == "c++" || mode == "cpp"){
+        editor.getSession().setMode("ace/mode/c_cpp");
+        return;
       }
-      if (sel[i].value == plchldr){
-        sel[i].style.color = '#909090';
-      }
-    }
-  }
-
-  setup_placeholder("#titlefield", POST_TITLE);
-  setup_placeholder("#tagfield", TAGS);
-  setup_placeholder(".cellinputname", CELL_NAME);
-  setup_placeholder(".cellinputcontent", CELL_CONTENT);
+      console.log("asking for mode change", editor);
+      editor.getSession().setMode("ace/mode/" + mode);
+  }).val("text").css("color", "inherit").css("border-color", "#409060").attr("disabled", true);
+  //setup_placeholder(".cellinputcontent", CELL_CONTENT);
 }
