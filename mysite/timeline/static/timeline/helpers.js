285cd922279ce77ce9af3c486d35e27b5a170429
User <user@Mohameds-iMac.local>
2016-12-22 15:22:06 +0400

    Refactored setup_placeholder function

---
+++ mysite/timeline/static/timeline/helpers.js
@@ -0,0 +1,16 @@
+function setup_placeholder(selector, plchldr){
+  $(selector).focus(function(){
+    if (this.value == plchldr){
+      this.value = "";
+    }
+    $(this).css('color', 'inherit');
+  }).focusout(function(){
+    if (this.value == ""){
+      $(this).css('color', '#909090');
+      this.value = plchldr;
+    }
+  })
+
+    $(selector).val(plchldr);
+    $(selector).css("color", "#909090");
+}
